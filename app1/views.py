14e7ecce7d5bab8ad8a619fdf1c6916cddf3f143
khaled-eem <khaled.3.mm@hotmail.com>
2024-05-11 05:04:50 +0300

    sec push

--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from .forms import *
 # Create your views here.
 def login_page(request):
-    return render(request,'app1\login.html')
+    if request.method=='POST':
+        form=LoginForm(request.POST) 
+
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            print(username)
+
+            return HttpResponseRedirect(reverse('register'))
+
+        else:
+                return render(request,'app1\login.html',{
+                'form':LoginForm()})
+
+    return render(request,'app1\login.html',{
+                'form':LoginForm()})
+    
 
 
 def register_page(request):
